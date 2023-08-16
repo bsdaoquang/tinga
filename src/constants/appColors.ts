a692b34b74b32096b92aa56aa7f8c6dbf8917a2c
bsdaoquang <bsdaoquang@gmail.com>
2023-08-16 10:10:15 +0700

    bottom done

--- src/constants/appColors.ts
+++ src/constants/appColors.ts
@@ -1,4 +1,9 @@
 export const appColors = {
   white: '#fff',
-  bgColor: '#f8f8f8'
+  bgColor: '#f8f8f8',
+  primary: '#32645B',
+  gray: '#AEACAE',
+  text: '#263238',
+  success: '#13917B',
+  success1: '#ABC43F'
 };
--- src/routers/TabNavigator.tsx
+++ src/routers/TabNavigator.tsx
@@ -1,11 +1,19 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import {
+  Element3,
+  Home,
+  SearchNormal1,
+  ShoppingCart,
+  User,
+} from 'iconsax-react-native';
 import React from 'react';
-import HomeNavigator from './HomeNavigator';
+import {appColors} from '../constants/appColors';
 import ExploreNavigator from './ExploreNavigator';
 import GroceryNavigator from './GroceryNavigator';
+import HomeNavigator from './HomeNavigator';
 import ProfileNavigator from './ProfileNavigator';
-import {appColors} from '../constants/appColors';
+import {Text} from 'react-native';
 
 const TabNavigator = () => {
   const Tabs = createBottomTabNavigator();
@@ -13,7 +21,7 @@ const TabNavigator = () => {
   return (
     <NavigationContainer>
       <Tabs.Navigator
-        screenOptions={{
+        screenOptions={({route}) => ({
           headerShown: false,
           tabBarStyle: {
             borderTopLeftRadius: 12,
@@ -22,7 +30,41 @@ const TabNavigator = () => {
             borderTopWidth: 0,
             backgroundColor: appColors.white,
           },
-        }}>
+          tabBarLabel({focused, color}) {
+            return (
+              <Text
+                style={{
+                  fontSize: 12,
+                  fontWeight: '500',
+                  marginBottom: 4,
+                  color: focused ? appColors.primary : appColors.gray,
+                }}>
+                {route.name}
+              </Text>
+            );
+          },
+          tabBarIcon: ({focused, size, color}) => {
+            let icon;
+            size = 22;
+            color = focused ? appColors.primary : appColors.gray;
+            if (route.name === 'Home') {
+              icon = (
+                <Element3
+                  size={size}
+                  color={color}
+                  variant={focused ? 'Bold' : 'Outline'}
+                />
+              );
+            } else if (route.name === 'Explore') {
+              icon = <SearchNormal1 size={size} color={color} />;
+            } else if (route.name === 'Grocery') {
+              icon = <ShoppingCart size={size} color={color} />;
+            } else {
+              icon = <User size={size} color={color} />;
+            }
+            return icon;
+          },
+        })}>
         <Tabs.Screen name="Home" component={HomeNavigator} />
         <Tabs.Screen name="Explore" component={ExploreNavigator} />
         <Tabs.Screen name="Grocery" component={GroceryNavigator} />
