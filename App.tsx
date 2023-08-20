5923e14296794d04f529a42fc574d2f2f7e26ed0
bsdaoquang <bsdaoquang@gmail.com>
2023-08-20 10:04:11 +0700

    run on mac ok

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
 import Router from './src/routers/router';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router />
+      <GestureHandlerRootView style={{flex: 1}}>
+        <Router />
+      </GestureHandlerRootView>
     </Provider>
   );
 };
