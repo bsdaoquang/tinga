0c9c387ba3512868f2bb1817804f29f5fd664434
bsdaoquang <bsdaoquang@gmail.com>
2023-08-21 16:45:56 +0700

    profile

--- App.tsx
+++ App.tsx
@@ -3,12 +3,15 @@ import {Provider} from 'react-redux';
 import store from './src/redux/store';
 import Router from './src/routers/router';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {Host} from 'react-native-portalize';
 
 const App = () => {
   return (
     <Provider store={store}>
       <GestureHandlerRootView style={{flex: 1}}>
-        <Router />
+        <Host>
+          <Router />
+        </Host>
       </GestureHandlerRootView>
     </Provider>
   );
