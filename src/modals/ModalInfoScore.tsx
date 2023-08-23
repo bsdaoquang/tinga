27ead0f54c0c44cfcaa5854e7bcd016802b63635
bsdaoquang <bsdaoquang@gmail.com>
2023-08-23 07:13:37 +0700

    modal

--- src/modals/ModalInfoScore.tsx
+++ src/modals/ModalInfoScore.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useRef} from 'react';
 import {Modalize} from 'react-native-modalize';
-import {RowComponent, TextComponent} from '../components';
+import {RowComponent, TextComponent, TitleComponent} from '../components';
 import {Portal} from 'react-native-portalize';
-import {View} from 'react-native';
+import {Image, ScrollView, View} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {appColors} from '../constants/appColors';
 
@@ -39,6 +39,21 @@ const ModalInfoScore = (props: Props) => {
           <RowComponent justify="flex-end" onPress={handleCloseModal}>
             <AntDesign name="close" color={appColors.gray} size={22} />
           </RowComponent>
+          <ScrollView>
+            <TitleComponent size={20} text="What is a list score?" />
+            <TextComponent
+              size={14}
+              text="Your grocery list score is a number between 0 and 100 that gives you insight into the food choices you make when shopping. ‘Green’ food items will boost you score the most, while red items won’t. At Tinga, we recommend aiming for a score between 70-100, the more green (great choices) the better!"
+            />
+            <Image
+              source={require('../assets/images/demoscore.png')}
+              style={{}}
+            />
+            <TextComponent
+              size={14}
+              text="Your grocery list score is a number between 0 and 100 that gives you insight into the food choices you make when shopping. ‘Green’ food items will boost you score the most, while red items won’t. At Tinga, we recommend aiming for a score between 70-100, the more green (great choices) the better!"
+            />
+          </ScrollView>
         </View>
       </Modalize>
     </Portal>
