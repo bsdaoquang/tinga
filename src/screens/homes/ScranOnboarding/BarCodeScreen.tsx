d592833a89c3e7dc37abe015034c451d69ff30e1
bsdaoquang <bsdaoquang@gmail.com>
2023-09-13 13:05:29 +0700

    add qr code done

--- src/screens/homes/ScranOnboarding/BarCodeScreen.tsx
+++ src/screens/homes/ScranOnboarding/BarCodeScreen.tsx
@@ -1,11 +1,38 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, Platform, PermissionsAndroid} from 'react-native';
+import React, {useEffect} from 'react';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 const BarCodeScreen = () => {
+  useEffect(() => {
+    requestPermision();
+  }, []);
+
+  const requestPermision = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.CAMERA,
+          {
+            title: 'RequestAuth',
+            message: 'Please allow camera permission to scan QR code',
+            buttonNeutral: 'Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'Agree',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('Đã được cấp quyền');
+        } else {
+          console.log('Yêu cầu bị từ chối');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
-    <View>
-      <Text>BarCodeScreen</Text>
-    </View>
+    <View>{/* <QRCodeScanner onRead={val => console.log(val)} /> */}</View>
   );
 };
 
