d6f94c04b6c84dab9b4c81d0545d738e128a1810
quangdv <quangdv@vntts.com.vn>
2023-10-26 16:03:31 +0700

    fix login

--- App.tsx
+++ App.tsx
@@ -12,7 +12,6 @@ import {SplashScreen} from './src/screens';
 let codePushOptions = {checkFrequency: codePush.CheckFrequency.MANUAL};
 
 const App = () => {
-  const [isWelcome, setIsWelcome] = useState(true);
   const {stop} = useTourGuideController();
 
   useEffect(() => {
@@ -25,43 +24,25 @@ const App = () => {
       },
       installMode: codePush.InstallMode.IMMEDIATE,
     });
-
-    getProfile();
   }, []);
-
-  const getProfile = async () => {
-    const api = `/getUserProfile`;
-    try {
-      await handleGetData.handleUser(api).then((res: any) => {
-        setIsWelcome(false);
-      });
-    } catch (error) {
-      console.log(error);
-      setIsWelcome(false);
-    }
-  };
 
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <Provider store={store}>
         <Host>
-          {!isWelcome ? (
-            <TourGuideProvider
-              {...{borderRadius: 16}}
-              tooltipStyle={{
-                direction: 'ltr',
-                flexDirection: 'row',
-              }}
-              labels={{
-                finish: 'Close',
-              }}
-              tooltipComponent={() => null}
-            >
-              <Router />
-            </TourGuideProvider>
-          ) : (
-            <SplashScreen />
-          )}
+          <TourGuideProvider
+            {...{borderRadius: 16}}
+            tooltipStyle={{
+              direction: 'ltr',
+              flexDirection: 'row',
+            }}
+            labels={{
+              finish: 'Close',
+            }}
+            tooltipComponent={() => null}
+          >
+            <Router />
+          </TourGuideProvider>
         </Host>
       </Provider>
     </GestureHandlerRootView>
--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,16 +1,67 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {NavigationContainer} from '@react-navigation/native';
-import React, {useEffect} from 'react';
-import {useSelector} from 'react-redux';
-import {authSelector} from '../redux/reducers/authReducer';
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import profileAPI from '../apis/userAPI';
+import {appInfos} from '../constants/appInfos';
+import {addAuth, authSelector} from '../redux/reducers/authReducer';
+import {SplashScreen} from '../screens';
 import MainNavigator from './MainNavigator';
 import WelcomeNavigator from './WelcomeNavigator';
 
 const Router = () => {
+  const [isWelcome, setIsWelcome] = useState(true);
   const auth = useSelector(authSelector);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    handleCheckUserLoginAgain();
+  }, []);
+
+  const handleCheckUserLoginAgain = async () => {
+    const api = `/getUserProfile`;
+    const apiChoces = '/getUserChoice';
+
+    const resLocal = await AsyncStorage.getItem(
+      appInfos.localDataName.userData,
+    );
+
+    try {
+      const userData = resLocal ? JSON.parse(resLocal) : {};
+
+      await profileAPI.HandleUser(apiChoces).then(async (res: any) => {
+        if (res.diets && res.diets.length > 0) {
+          await profileAPI.HandleUser(api).then((res: any) => {
+            if (userData && res.id) {
+              dispatch(
+                addAuth({
+                  ...userData,
+                  ...res,
+                }),
+              );
+            } else {
+              dispatch(addAuth({}));
+            }
+
+            setIsWelcome(false);
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      setIsWelcome(false);
+    }
+  };
 
   return (
     <NavigationContainer>
-      {auth.access_token ? <MainNavigator /> : <WelcomeNavigator />}
+      {isWelcome ? (
+        <SplashScreen />
+      ) : auth.access_token ? (
+        <MainNavigator />
+      ) : (
+        <WelcomeNavigator />
+      )}
     </NavigationContainer>
   );
 };
